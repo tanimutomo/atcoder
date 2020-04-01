297a2cc3c7e7b41fc3dccba8b9f51e042933d2e2
tanimutomo <tanimutomo@gmail.com>
2020-04-01 15:22:55 +0900

    Add bfs d

--- abc160/d_.py
+++ abc160/d_.py
@@ -1,31 +1,32 @@
 n, x, y = map(int, input().split())
 x, y = x-1, y-1
 
-am = list()
-for i in range(n):
-    if i == 0:
-        ads = [i+1]
-    elif i == n-1:
-        ads = [i-1]
-    else:
-        ads = [i-1, i+1]
-    if i == x:
-        ads.append(y)
-    elif i == y:
-        ads.append(x)
-    am.append(ads)
+
+def update(dist, queue, d, x):
+    if dist[x] >= 0: return dist, queue
+    dist[x] = d+1
+    queue.append(x)
+    return dist, queue
 
 ans = [0 for _ in range(n)]
 for i in range(n):
-    queue = am[i]
-    done = [False for _ in range(n)]
-    d = 0
+    dist = [-1 for _ in range(n)]
+    dist[i] = 0
+    queue = [i]
     while True:
         if len(queue) == 0: break
         now = queue.pop(0)
-        if done[now]: continue
+        d = dist[now]
+        if now-1 >= 0:
+            dist, queue = update(dist, queue, d, now-1)
+        if now+1 < n:
+            dist, queue = update(dist, queue, d, now+1)
+        if now == x:
+            dist, queue = update(dist, queue, d, y)
+        if now == y:
+            dist, queue = update(dist, queue, d, x)
+    for d in dist:
         ans[d] += 1
-        done[now] = True
-        queue.extend(am[now])
 
-print(ans)
+for a in ans[1:]:
+    print(a//2)
